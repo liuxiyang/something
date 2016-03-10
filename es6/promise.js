b9132e4aece66d389ee132061ddab8c3420a174f
liuxiyang <liuxiyang@jd.com>
2016-03-10 14:18:10 +0800

    add es6

---
+++ es6/promise.js
@@ -0,0 +1,40 @@
+var getJSON = function(url) {
+  var promise = new Promise(function(resolve, reject){
+    var client = new XMLHttpRequest();
+    client.open("GET", url);
+    client.onreadystatechange = handler;
+    client.responseType = "json";
+    client.setRequestHeader("Accept", "application/json");
+    client.send();
+
+    function handler() {
+      if ( this.readyState !== 4 ) {
+        return;
+      }
+      if (this.status === 200) {
+        resolve(this.response);   //除了正常的值意外，还能返回另一个promise实例
+      } else {
+        reject(new Error(this.statusText));  //一般为error实例
+      }
+    };
+  });
+
+  return promise;
+};
+
+getJSON("/posts.json").then(function(json) {
+  console.log('Contents: ' + json);
+}, function(error) {
+  console.error('出错了', error);
+});
+
+
+// p1是一个Promise，3秒之后变为rejected。p2的状态由p1决定，1秒之后，p2调用resolve方法，但是此时p1的状态还没有改变，因此p2的状态也不会变。又过了2秒，p1变为rejected，p2也跟着变为rejected
+var p1 = new Promise(function (resolve, reject) {
+  setTimeout(() => reject(new Error('fail')), 3000)
+})
+var p2 = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve(p1), 1000)
+})
+p2.then(result => console.log(result))
+p2.then(function(result){console.log(result)})
